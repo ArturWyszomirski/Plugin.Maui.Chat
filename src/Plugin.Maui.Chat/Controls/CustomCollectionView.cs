e32c06f3fdbf8f7a10d1bbdca428ce414259fef6
Mateusz Łaś <mateuszlas3@gmail.com>
2024-09-23 01:28:57 +0200

    KeepLastItemInView it is bugged on Windows /maui/issues/17369

--- src/Plugin.Maui.Chat/Controls/CustomCollectionView.cs
+++ src/Plugin.Maui.Chat/Controls/CustomCollectionView.cs
@@ -5,11 +5,21 @@ public class CustomCollectionView : CollectionView
 
     public void TurnOnScrollToLastItem()
     {
+#if !WINDOWS
         this.ItemsUpdatingScrollMode = ItemsUpdatingScrollMode.KeepLastItemInView;
+#else
+
+        // TODO
+#endif
     }
     
     public void TurnOffScrollToLastItem()
     {
+#if !WINDOWS
         this.ItemsUpdatingScrollMode = ItemsUpdatingScrollMode.KeepScrollOffset;
+#else
+        /// TODO
+#endif
+
     }
 }
