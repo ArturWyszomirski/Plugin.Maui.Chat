50c8c18a79aeeb683875cd25d5b84a6de18935b8
Mateusz Łaś <mlasapk@gmail.com>
2024-09-28 18:02:49 +0200

    Updated RequestPermission

--- src/Plugin.Maui.Chat/Services/SpeechToTextService.cs
+++ src/Plugin.Maui.Chat/Services/SpeechToTextService.cs
@@ -10,13 +10,9 @@ internal class SpeechToTextService(Controls.Chat chat)
     {
         string text = string.Empty;
 
-        if (await Permissions.RequestAsync<Permissions.Microphone>() != PermissionStatus.Granted)
-        {
-            await Shell.Current.DisplayAlert("Permission denied", "The app needs microphone permission to record audio.", "OK");
-            chat.AudioRecorderColor = chat.SecondaryColor;
+        if (await RequestPermission() is not PermissionStatus.Granted)
             return text;
-        }
-
+        
         if (!chat.IsRecording) // couldn't use SpeechToText.Default.CurrentState because is always in Listenig state on Windows OS
         {
             cancelSpeechToTextTokenSource = new();
@@ -58,6 +54,27 @@ internal class SpeechToTextService(Controls.Chat chat)
 #endif
 
         return text;
+    }
+
+    private async Task<PermissionStatus> RequestPermission()
+    {
+        if (await Permissions.RequestAsync<Permissions.Microphone>() != PermissionStatus.Granted)
+        {
+            await Shell.Current.DisplayAlert("Permission denied", "The app needs microphone permission to record audio.", "OK");
+            chat.AudioRecorderColor = chat.SecondaryColor;
+            return PermissionStatus.Denied;
+        }
+        
+#if IOS || MACCATALYST
+        var isGranted = await SpeechToText.Default.RequestPermissions(CancellationToken.None);
+        if (!isGranted)
+        {
+            await Shell.Current.DisplayAlert("Permission denied", "This feature requires access to speech recognition.", "OK");
+            chat.AudioRecorderColor = chat.SecondaryColor;
+            return PermissionStatus.Denied;
+        }
+#endif
+        return PermissionStatus.Granted;
     }
 
     static string CapitalizeFirstLetter(string text) => !string.IsNullOrWhiteSpace(text) ? char.ToUpper(text[0]) + text.Substring(1) : text;
